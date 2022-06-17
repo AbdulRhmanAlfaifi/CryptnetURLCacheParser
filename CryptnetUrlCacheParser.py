c6d4133b6586d9012cd41df7c593df3e4734e6a9
AbdulRhman Alfaifi <a.alfaifi.14@gmail.com>
2022-06-17 18:25:33 +0300

    Change hash to upper case

--- CryptnetUrlCacheParser.py
+++ CryptnetUrlCacheParser.py
@@ -56,7 +56,7 @@ class CertutilCacheParser:
             with open(fname, "rb") as f:
                 for chunk in iter(lambda: f.read(4096), b""):
                     hash_md5.update(chunk)
-            return hash_md5.hexdigest()
+            return hash_md5.hexdigest().upper()
         except:
             return "00000000000000000000000000000000"
 
